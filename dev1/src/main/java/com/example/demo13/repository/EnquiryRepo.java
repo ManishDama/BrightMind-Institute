19d19c08741d70f28df14a14a18efc64dd4e54e9
ManishDama <121088396+ManishDama@users.noreply.github.com>
2025-08-25 17:40:34 +0530

    Delete dev1 directory

--- dev1/src/main/java/com/example/demo13/repository/EnquiryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo13.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo13.entity.Enquiry;
-
-public interface EnquiryRepo  extends JpaRepository<Enquiry, Long>{
-
-}
